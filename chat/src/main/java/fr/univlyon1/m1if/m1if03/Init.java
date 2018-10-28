d483752e8ab11506b324ef569cfe068bfaff0061
ElodieMtc <jesselo1@hotmail.fr>
2018-10-28 20:22:27 +0100

    TP2 - 2.2

--- chat/src/main/java/fr/univlyon1/m1if/m1if03/Init.java
+++ chat/src/main/java/fr/univlyon1/m1if/m1if03/Init.java
@@ -5,36 +5,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/Init") 
+@WebServlet("/Init")
 public class Init extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
 
         super.init(config);
-        /*response.setContentType("text/html");
-
-        response.setCharacterEncoding( "UTF-8" );
-
-        PrintWriter out = response.getWriter();
-
-        out.println("<!DOCTYPE html>");
-
-        out.println("<html>");
-
-        out.println("<head>");
-
-        out.println("<meta charset=\"utf-8\" />");
-
-        out.println("<title>Test</title>");
-
-        out.println("</head>");
-
-        out.println("<body>");
-
-        out.println("<p>Ceci est une page générée depuis une servlet.</p>");
-
-        out.println("</body>");
-
-        out.println("</html>");*/
 
     }
 
