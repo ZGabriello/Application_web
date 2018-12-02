6380610f732d5ebe0c5efd33b5bd73ec37d7dc9e
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2018-12-02 20:06:06 +0100

    update style.css

---
+++ chat/src/main/java/fr/univlyon1/m1if/m1if03/tp4/controller/UserController.java
@@ -0,0 +1,72 @@
+package fr.univlyon1.m1if.m1if03.tp4.controller;
+
+import fr.univlyon1.m1if.m1if03.tp2.Modele.GestionMessages;
+import fr.univlyon1.m1if.m1if03.tp2.Modele.Message;
+import fr.univlyon1.m1if.m1if03.tp3.beans.GestionUsersBean;
+import fr.univlyon1.m1if.m1if03.tp3.beans.HelloBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/salon/*")
+public class UserController {
+    private HelloBean helloBean;
+    private GestionMessages gestionMessages;
+    private GestionUsersBean gestionUsers;
+
+    @Autowired
+    public UserController(GestionMessages gestionMessages, GestionUsersBean gestionUsers) {
+
+        this.gestionMessages = gestionMessages;
+        this.gestionUsers = gestionUsers;
+    }
+
+    @GetMapping("/listeMessages")
+    public ModelAndView listeMessages(@RequestParam(name="name") String name) {
+        Map<String, ArrayList<Message>> map = new HashMap<String, ArrayList<Message>>();
+        map.put("messages", gestionMessages.getMessagesList(name));
+        return new ModelAndView("listeMessages", map);
+    }
+
+    @GetMapping("/nbMessages")
+    public ModelAndView nbMessages(@RequestParam(name="name") String name) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("nombre", gestionMessages.getMessageNumber(name));
+        return new ModelAndView("nbMessages", map);
+    }
+
+    @GetMapping("/messagesUlterieur")
+    public ModelAndView messagesUlterieur(@RequestParam(name="name") String name,@RequestParam(name="num") int num) {
+        Map<String, ArrayList<Message>> map = new HashMap<String, ArrayList<Message>>();
+        map.put("messages", gestionMessages.getMessagesListAfter(name,num));
+        return new ModelAndView("listeMessages", map);
+    }
+/*
+    @PostMapping("/users")
+    public UserBean user(@ModelAttribute UserCreateRequest request) {
+        UserBean user = new UserBean();
+        gestionUsers.addUser(user);
+        return user;
+    }
+
+    @GetMapping("/message")
+    public ModelAndView message(@RequestParam(name="name") String name, @RequestParam(name="num") int num) {
+        Map<String, String> map = new HashMap<String, String>();
+        ArrayList<Message> liste = gestionMessages.getMessagesList(name);
+        for (Message m : liste) {
+            if (m.getNum() == num) {
+                map.put("message", m.getTexte());
+            }
+        }
+
+        return new ModelAndView("contenuMessage", map);
+    }*/
+
+}
+
